2ef6b708a224fe94ae673d14740a0725e4e7db94
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-06-06 13:39:22 +0200

    #23 Add custom html to wordpress menu | How to make html menu dynamic in wordprress | wp_nav_menu

--- inc/classes/class-menus.php
+++ inc/classes/class-menus.php
@@ -10,7 +10,7 @@ namespace AQUILA_THEME\Inc;
 use  AQUILA_THEME\Inc\Traits\Singleton;
 
 class Menus {
-    
+
     use Singleton;
 
     protected function __construct() {
@@ -31,5 +31,15 @@ class Menus {
             'aquila-header-menu' => esc_html__( 'Header Menu', 'aquila' ),
             'aquila-footer-menu' => esc_html__( 'Footer Menu', 'aquila' )
         ]);
+    }
+
+    public function get_menu_id( $location ) {
+        // Get all the locations.
+        $locations = get_nav_menu_locations();
+
+        // Get object id by location.
+        $menu_id = $locations[$location];
+
+        return ! empty( $menu_id ) ? $menu_id : '';
     }
 }
